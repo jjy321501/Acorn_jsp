213d2d59ef6d82954b39418c8a2c8546cc5689fe
jjugit config --global user.name jjy321501git config --global user.email jjyu. <jjy321501@naver.com>
2021-01-14 17:18:34 +0900

    Step06_Final detail.jsp 이전 다음 기능

--- Step06_Final/WebContent/gallery/detail.jsp
+++ Step06_Final/WebContent/gallery/detail.jsp
@@ -3,11 +3,11 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-	//자세히 보여줄 갤러리 item 번호를 읽어온다
+	//자세히 보여줄 겔러리 item 번호를 읽어온다.
 	int num=Integer.parseInt(request.getParameter("num"));
-	//번호를 이용해서 갤러리 item 정보를 얻어온다
+	//번호를 이용해서 겔러리 item 정보를 얻어온다.
 	GalleryDto dto=GalleryDao.getInstance().getData(num);
-%>
+%>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,12 +16,20 @@
 </head>
 <body>
 <div class="container">
-	<img src="${pageContext.request.contextPath}<%=dto.getImagePath() %>"/>
 	<div>
-		<p><%=dto.getCaption() %></p>
-		<p>by <strong><%=dto.getWriter() %></strong></p>
-		<p><%=dto.getRegdate() %></p>
+		<img src="${pageContext.request.contextPath }<%=dto.getImagePath()%>"/>
+		<div>
+			<p><%=dto.getCaption() %></p>
+			<p>by <strong><%=dto.getWriter() %></strong></p>
+			<p><small><%=dto.getRegdate() %></small></p>
+		</div>
 	</div>
+	<%if(dto.getPrevNum() != 0){ %>
+		<a href="detail.jsp?num=<%=dto.getPrevNum()%>">이전</a>
+	<%} %>
+	<%if(dto.getNextNum() != 0){ %>
+		<a href="detail.jsp?num=<%=dto.getNextNum()%>">다음</a>
+	<%} %>
 </div>
 </body>
 </html>
--- Step06_Final/src/test/gallery/dto/GalleryDto.java
+++ Step06_Final/src/test/gallery/dto/GalleryDto.java
@@ -8,11 +8,12 @@ public class GalleryDto {
 	private String regdate;
 	private int startRowNum;
 	private int endRowNum;
-	//디폴트 생성자
+	private int prevNum; //이전글의 글번호
+	private int nextNum; //다음글의 글번호
+	//디폴트 생성자 
 	public GalleryDto() {}
-	
 	public GalleryDto(int num, String writer, String caption, String imagePath, String regdate, int startRowNum,
-			int endRowNum) {
+			int endRowNum, int prevNum, int nextNum) {
 		super();
 		this.num = num;
 		this.writer = writer;
@@ -21,64 +22,62 @@ public class GalleryDto {
 		this.regdate = regdate;
 		this.startRowNum = startRowNum;
 		this.endRowNum = endRowNum;
+		this.prevNum = prevNum;
+		this.nextNum = nextNum;
 	}
-
 	public int getNum() {
 		return num;
 	}
-
 	public void setNum(int num) {
 		this.num = num;
 	}
-
 	public String getWriter() {
 		return writer;
 	}
-
 	public void setWriter(String writer) {
 		this.writer = writer;
 	}
-
 	public String getCaption() {
 		return caption;
 	}
-
 	public void setCaption(String caption) {
 		this.caption = caption;
 	}
-
 	public String getImagePath() {
 		return imagePath;
 	}
-
 	public void setImagePath(String imagePath) {
 		this.imagePath = imagePath;
 	}
-
 	public String getRegdate() {
 		return regdate;
 	}
-
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-
 	public int getStartRowNum() {
 		return startRowNum;
 	}
-
 	public void setStartRowNum(int startRowNum) {
 		this.startRowNum = startRowNum;
 	}
-
 	public int getEndRowNum() {
 		return endRowNum;
 	}
-
 	public void setEndRowNum(int endRowNum) {
 		this.endRowNum = endRowNum;
 	}
-	
-	
+	public int getPrevNum() {
+		return prevNum;
+	}
+	public void setPrevNum(int prevNum) {
+		this.prevNum = prevNum;
+	}
+	public int getNextNum() {
+		return nextNum;
+	}
+	public void setNextNum(int nextNum) {
+		this.nextNum = nextNum;
+	}
 	
 }
